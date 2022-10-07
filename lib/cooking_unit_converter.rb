d542e270686a473954988446d7128560bc57d765
unknown <user@mil8a-202b-09.mmcs.sfedu.ru>
2022-10-07 14:22:47 +0300

    Initial commit

---
+++ lib/cooking_unit_converter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cooking_unit_converter/version"
+
+module CookingUnitConverter
+  class Error < StandardError; end
+  # Your code goes here...
+end
